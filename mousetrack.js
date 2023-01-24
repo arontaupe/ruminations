fe48ef1d2383880735662a9db411dff1aba0c895
arontaupe <aron@petau.net>
2023-01-24 17:38:55 +0100

    working crawler with scrapy

--- mousetrack.js
+++ mousetrack.js
@@ -1,7 +1,4 @@
 function getCursorPosition(event) {
-
 document.getElementById("c_p_x").textContent = event.clientX;
-
 document.getElementById("c_p_y").textContent = event.clientY;
-
 }
---
+++ url_generator.py
@@ -0,0 +1,22 @@
+import requests
+import pandas as pd
+
+
+HEADERS = ({'User-Agent':
+            'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36',
+            'Accept-Language': 'en-US, en;q=0.5'})
+
+asins = pd.read_csv('asins.csv')
+
+for link in range(len(asins)):
+    base_url = "https://www.amazon.com/"
+    static = 'dp/'
+    asin = asins.asin[0]
+    url = base_url + static + asin
+    #print(url)
+    html = requests.get(url, headers=HEADERS)
+    if html.status_code != 404:
+        print(url)
+        print(html.status_code)
+    else:
+        print("-", end='')
